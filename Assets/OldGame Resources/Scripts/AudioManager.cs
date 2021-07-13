994897f1d69ef65d0e8176cfe85c7562653c3c0c
ZionJones <Joneszion412@gmail.com>
2021-07-12 23:59:53 -0700

    added some possible reosurces we can use and added cinemachine to project

---
+++ Assets/OldGame Resources/Scripts/AudioManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+        private static readonly string FirstPlay = "FirstPlay";
+        private static readonly string BackgroundPref = "BackgroundPref";
+        private static readonly string SoundEffectsPref = "SoundEffectsPRef";
+        
+        private int firstPlayInt;
+        public Slider backgroundSlider, soundEffectSlider;
+        public float backgroundFloat, soundEffectFloat;
+
+        public AudioSource backgroundAudio;
+        public AudioSource[] soundEffectAudio; //can set to however many audio sources you need assigned in GUI
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        firstPlayInt = PlayerPrefs.GetInt(FirstPlay);
+
+        if(firstPlayInt == 0)
+        {
+            backgroundFloat = 0.75f;
+            soundEffectFloat = .25f;
+            backgroundSlider.value = backgroundFloat;   //adjust slider to adjust float value.
+            soundEffectSlider.value = soundEffectFloat;
+            
+            // Player prefs allow to save values threw diffrent scenes and menus. 
+            PlayerPrefs.SetFloat(BackgroundPref, backgroundFloat);  // saves background preference value to float value.
+            PlayerPrefs.SetFloat(SoundEffectsPref, soundEffectFloat);
+
+        }
+
+        else 
+        {
+            backgroundFloat = PlayerPrefs.GetFloat(BackgroundPref);
+            backgroundSlider.value = backgroundFloat;
+            soundEffectFloat = PlayerPrefs.GetFloat(SoundEffectsPref);
+            soundEffectSlider.value = soundEffectFloat;
+
+
+        }
+    }
+
+    public void SaveSoundSettings() 
+    {
+        PlayerPrefs.SetFloat(BackgroundPref, backgroundSlider.value);  // saves background preference value to float value.
+        PlayerPrefs.SetFloat(SoundEffectsPref, soundEffectSlider.value);
+    }
+
+    void OnApplicationFocus(bool inFocus) //focus is when the game screen is on and bieng in use by the player, essentalty the main window
+    {
+        if (!inFocus) 
+        {
+            SaveSoundSettings(); // whenever the game is exited (lose focused) the setting are automaticallyed saved works on android,desktop.
+        }
+    
+    }
+    
+    public void UpdateSound() // set OnValue setting to this in slider GUI 
+    {
+        backgroundAudio.volume = backgroundSlider.value;
+
+        for(int i= 0;  i < soundEffectAudio.Length; i++) 
+        {
+            soundEffectAudio[i].volume = soundEffectSlider.value;
+        }
+    
+    }
+
+
+
+}
---
+++ Assets/OldGame Resources/Scripts/Timer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Timer : MonoBehaviour
+{
+    
+    [Header("Timer")]
+    public GameObject textDisplay;
+    public GameObject canvas;
+    public float secondsLeft = 30.0f;
+    public bool takingAway = false;
+
+    
+    
+    void Start() 
+    { 
+        textDisplay.GetComponent<Text>().text = "00" + secondsLeft;
+        //my_Movement.GetComponent<MovementController>();
+    
+    
+    }
+        
+    
+    void Update() 
+    {
+
+        if(takingAway == false && secondsLeft > 0) 
+        {
+            StartCoroutine(TimerTake());
+            
+        }
+
+       
+   }
+
+    IEnumerator TimerTake()
+    {
+        takingAway = true;
+        yield return new WaitForSeconds(1);
+        secondsLeft -= 1;
+
+        if (secondsLeft < 10)
+        {
+            textDisplay.GetComponent<Text>().text = "00:0" + secondsLeft;
+            if (secondsLeft == 0)
+            {
+                //speed = 0f;
+
+            }
+
+        }
+        else
+        {
+            textDisplay.GetComponent<Text>().text = "00:0" + secondsLeft;
+        }
+        takingAway = false;
+    }
+}
