1a487fc5ff3ddc307bbd29965cb88d8556564037
Sean A <Hammytaro@users.noreply.github.com>
2021-11-09 20:04:46 -0800

    ui

---
+++ Assets/Quit_to_Main.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Quit_to_Main : MonoBehaviour
+{
+    public GameObject quitMain;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+   
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+}
