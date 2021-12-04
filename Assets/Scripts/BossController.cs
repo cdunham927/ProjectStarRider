a166f71e82a1b7ab8eaedb725284c541994c3465
cdunham927 <cdunham927@gmail.com>
2021-12-04 01:53:59 -0800

    Started Boss

--- Assets/Scripts/BossController.cs
+++ Assets/Scripts/BossController.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class BossController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Time between attacks
+    public float timeBetweenAttacks;
+    //Range for when the enemy starts attacking
+    public float attackRange;
+    //Current cooldown time for attacking
+    float attackCools;
+    //Object pool for bullets to shoot
+    public ObjectPool bulletPool;
+    //Checks if the player is in range
+    public bool playerInRange = false;
+    //Set the radius for the detection collider
+    public SphereCollider detectionCollider;
+
+    private void OnEnable()
     {
-        
+        detectionCollider.radius = attackRange;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        //If the player is close enough
+        if (playerInRange)
+        {
+            //If the cooldown is at 0 then we can attack
+            if (attackCools <= 0) Attack();
+            //If the cooldown is greater than 0 we decrement it every frame
+            if (attackCools > 0) attackCools -= Time.deltaTime;
+        }
+    }
+
+    public void Attack()
+    {
+        //Get pooled bullet
+        GameObject bul = bulletPool.GetPooledObject();
+        if (bul != null)
+        {
+            //Put it where the enemy position is
+            bul.transform.position = transform.position;
+            //Aim it at the player
+            bul.transform.rotation = transform.rotation;
+            //Activate it at the enemy position
+            bul.SetActive(true);
+        }
+
+        //Reset attack cooldown
+        attackCools = timeBetweenAttacks;
     }
 }
