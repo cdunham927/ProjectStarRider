e876ae602f99dbccbb5b1e6d60fd812af5344b99
cdunham927 <cdunham927@gmail.com>
2022-09-13 19:54:41 -0700

    Fixed merge conflicts? Probably

--- Assets/Scripts/Player_Stats.cs
+++ Assets/Scripts/Player_Stats.cs
@@ -144,7 +144,7 @@ public class Player_Stats : MonoBehaviour
 
         healthImage.fillAmount = Mathf.Lerp(healthImage.fillAmount, (float)Curr_hp / (float)Max_hp, lerpSpd * Time.deltaTime);
         scoreText.text = "Score: " + Mathf.Round(score).ToString();
-        multiplierText.text = "Multiplier: " + Mathf.Round(scoreMultiplier) + "x";
+        multiplierText.text = "Multiplier: " + scoreMultiplier + "x";
         //innerRect.Width = ((float)Curr_hp / (float)Max_hp) * size;
     }
 
