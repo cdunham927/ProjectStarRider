a4a67c9af995cebac6670e96e78d69d97993d4fa
ZionJones <Joneszion412@gmail.com>
2022-09-21 20:57:19 -0700

    Revert "cole additions fix??"
    This reverts commit d97f61fa44f6faea6f678d5fa0254566ac07e01a.

--- Assets/Scripts/Enemies/BossHitPointController.cs
+++ Assets/Scripts/Enemies/BossHitPointController.cs
@@ -7,6 +7,9 @@ public class BossHitPointController : MonoBehaviour
     BossControllerBase boss;
     [Range(1, 3)]
     public float dmgMult;
+    public bool breakable = true;
+    float curDmg;
+    public float breakHp;
 
     private void Awake()
     {
@@ -15,9 +18,13 @@ public class BossHitPointController : MonoBehaviour
 
     public void Damage(int amt)
     {
+        //Calculate Damage
         int totDmg = Mathf.RoundToInt((float)amt * dmgMult);
         boss.Damage(totDmg);
-
+        //Blow up the weakpoint if it takes too much damage;
+        curDmg += totDmg;
+        if (curDmg >= breakHp) gameObject.SetActive(false);
+        //Retaliate against attack
         boss.retaliatePos = transform.position;
         boss.Retaliate();
     }
