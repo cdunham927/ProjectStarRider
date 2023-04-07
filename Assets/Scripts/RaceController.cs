085a766e288b94158c94e72485c5e9c2b7c05dab
cdunham927 <cdunham927@gmail.com>
2023-04-06 18:45:59 -0700

    Race level finished testing

--- Assets/Scripts/RaceController.cs
+++ Assets/Scripts/RaceController.cs
@@ -11,36 +11,67 @@ public class RaceController : MonoBehaviour
     public PlayerController player;
     public int checkpointsHit;
 
-    public Image playerImage;
-    public Image enemyImage;
+    public Slider playerImage;
+    public Slider enemyImage;
+
+    //How far the goal starts away from the player
+    float startDistance;
+    //Current distance to the goal, player and enemy
+    float curDistanceP;
+    float curDistanceE;
+
+    //Lerp speed of ui
+    public float lerpSpd = 7f;
+
+    public bool endofrace;
 
     private void Awake()
     {
         cont = FindObjectOfType<GameManager>();
-
         raceEnemy = FindObjectOfType<RaceEnemy>();
         player = FindObjectOfType<PlayerController>();
+        playerImage = GameObject.FindGameObjectWithTag("RaceProgressP").GetComponent<Slider>();
+        enemyImage = GameObject.FindGameObjectWithTag("RaceProgressE").GetComponent<Slider>();
 
         checkpointsHit = 0;
+        endofrace = false;
+
+        startDistance = 0;
+        for (int i = 0; i < checkpoints.Length - 1; i++)
+        {
+            if (i == 0) startDistance += Vector3.Distance(player.transform.position, checkpoints[i].transform.position);
+            else startDistance += Vector3.Distance(checkpoints[i].transform.position, checkpoints[i + 1].transform.position);
+        }
     }
 
     private void Update()
     {
-        //playerImage.fillAmount = ;
-        //enemyImage.fillAmount = ;
+        if (!endofrace)
+        {
+            //Player race progress
+            curDistanceP = Vector3.Distance(player.transform.position, checkpoints[checkpoints.Length - 1].transform.position);
+            playerImage.value = Mathf.InverseLerp(startDistance, 0f, curDistanceP);
+            //Enemy race progress
+            curDistanceE = Vector3.Distance(raceEnemy.transform.position, checkpoints[checkpoints.Length - 1].transform.position);
+            enemyImage.value = Mathf.InverseLerp(startDistance, 0f, curDistanceE);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player") && checkpointsHit >= checkpoints.Length)
+        if (other.CompareTag("Player") && checkpointsHit >= checkpoints.Length - 1)
         {
             cont.Victory();
             GetComponent<Collider>().enabled = false;
+            endofrace = true;
+            playerImage.value = 1;
         }
         if (other.CompareTag("RaceEnemy"))
         {
             cont.GameOver();
             GetComponent<Collider>().enabled = false;
+            endofrace = true;
+            enemyImage.value = 1;
         }
     }
 }
