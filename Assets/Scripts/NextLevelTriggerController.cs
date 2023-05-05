9bde83963dd06ed4c4b7fcfb24e398817cfb8c1a
ZionJones <Joneszion412@gmail.com>
2023-05-05 15:43:54 -0700

    build fixes

--- Assets/Scripts/NextLevelTriggerController.cs
+++ Assets/Scripts/NextLevelTriggerController.cs
@@ -5,18 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class NextLevelTriggerController : MonoBehaviour
 {
-    public LevelLoader lvl;
+    //public LevelLoader lvl;
 
     private void Awake()
     {
-        lvl = FindObjectOfType<LevelLoader>();
+        //lvl = FindObjectOfType<LevelLoader>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            lvl.LoadLevel(SceneManager.GetActiveScene().buildIndex + 1);
+            //lvl.LoadLevel(SceneManager.GetActiveScene().buildIndex + 1);
+            
+                //MusicController.instance.PlaySound();
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            
         }
     }
 }
