3af12ba870d5dc1b496867e03550386759665c18
cdunham927 <cdunham927@gmail.com>
2023-10-05 19:13:40 -0700

    Player select stuff

--- Assets/Scripts/PlayerSelect/PlayerSelectController.cs
+++ Assets/Scripts/PlayerSelect/PlayerSelectController.cs
@@ -1,27 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerSelectController : MonoBehaviour
 {
     public GameObject[] players;
+    public Sprite[] playerImages;
+    public Image curPlayerImage;
     int curSelected = 0;
 
     private void Awake()
     {
         curSelected = 0;
+        curPlayerImage.sprite = playerImages[curSelected];
     }
 
     public void Next()
     {
         if (curSelected < players.Length - 1) curSelected++;
         else curSelected = 0;
+
+        curPlayerImage.sprite = playerImages[curSelected];
     }
 
     public void Previous()
     {
         if (curSelected > 0) curSelected--;
         else curSelected = players.Length;
+
+        curPlayerImage.sprite = playerImages[curSelected];
     }
 
     public void Confirm()
