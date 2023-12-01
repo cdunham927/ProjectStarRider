c94ede10d1a2f1ed572959851125d8ce93c54fd6
cdunham927 <cdunham927@gmail.com>
2023-11-30 19:08:16 -0800

    Checkpoint controller edits

--- Assets/Scripts/CheckpointController.cs
+++ Assets/Scripts/CheckpointController.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class CheckpointController : MonoBehaviour
 {
     RaceController race;
+    public enum effect { none, spawnEnemy, spikes, pillars, obstacleWalls, openDoor }
+    public effect checkpointEffect;
+
+    public bool hasActivated = false;
+
+    public GameObject[] enemies;
 
     private void Awake()
     {
+        hasActivated = false;
         race = FindObjectOfType<RaceController>();
     }
 
@@ -17,6 +24,32 @@ public class CheckpointController : MonoBehaviour
         {
             race.checkpointsHit++;
             this.enabled = false;
+        }
+
+        switch(checkpointEffect)
+        {
+            case effect.none:
+                hasActivated = true;
+                break;
+            case effect.spawnEnemy:
+                foreach (GameObject t in enemies)
+                {
+                    t.SetActive(true);
+                }
+                hasActivated = true;
+                break;
+            case effect.spikes:
+                hasActivated = true;
+                break;
+            case effect.pillars:
+                hasActivated = true;
+                break;
+            case effect.obstacleWalls:
+                hasActivated = true;
+                break;
+            case effect.openDoor:
+                hasActivated = true;
+                break;
         }
     }
 }
