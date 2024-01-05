bdbbc1d5e10a77df0e16c57555f14df7b0858e33
cdunham927 <cdunham927@gmail.com>
2024-01-05 01:35:42 -0800

    Enemy Manager : Trap Controller implemented in race test level

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -8,7 +8,8 @@ public class EnemyManager : MonoBehaviour
     public bool HasSpawned;
     //Locked rooms
     public bool hasClosed = false;
-    public MovingWalls door;
+    //public MovingWalls door;
+    public GameObject[] walls;
     int enemyCount;
     GameManager cont;
 
@@ -27,34 +28,48 @@ public class EnemyManager : MonoBehaviour
     //Enemy notices player and makes sure they are there when spawned. Enemies spawn when player comes close or triggers.
     public void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player") && HasSpawned == (false))
+        if(other.CompareTag("Player") && HasSpawned == false)
         {
             for(int i = 0; i < enemies.Length; i++)
             {
                 enemies[i].SetActive(true);
             }
-            HasSpawned = true;
 
-            if (door != null && !hasClosed)
+            //if (door != null && !hasClosed)
+            //{
+            //    hasClosed = true;
+            //    Close();
+            //}
+
+            foreach (GameObject w in walls)
             {
-                hasClosed = true;
-                Close();
+                w.SetActive(true);
             }
+
+            HasSpawned = true;
+            hasClosed = true;
         }
     }
        
     public void EnemyDied()
     {
         enemyCount--;
-        if (enemyCount <= 0 && door != null)
+        //if (enemyCount <= 0 && door != null)
+        //{
+        //    door.SwitchDoor();
+        //}
+        if (enemyCount <= 0 && walls.Length > 0)
         {
-            door.SwitchDoor();
+            foreach (GameObject w in walls)
+            {
+                w.SetActive(false);
+            }
         }
         cont.EnemyDiedEvent();
     }
 
     public void Close()
     {
-        door.SwitchDoor();
+        //door.SwitchDoor();
     }
 }
