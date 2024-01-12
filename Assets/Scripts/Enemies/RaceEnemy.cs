39d6e1323079825b5ff2337064849fa8fb03905b
cdunham927 <cdunham927@gmail.com>
2024-01-11 18:59:07 -0800

    Fixed race level and race enemy

--- Assets/Scripts/Enemies/RaceEnemy.cs
+++ Assets/Scripts/Enemies/RaceEnemy.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(AStarAgent))]
 public class RaceEnemy : MonoBehaviour
 {
     public GameObject[] waypoints;
@@ -16,38 +15,45 @@ public class RaceEnemy : MonoBehaviour
     [SerializeField] Transform pointA;
     [SerializeField] Transform pointB;
 
+    public Vector3 offset;
+    float distance;
+
+    public float turnSpd;
+
     private void Start()
     {
         //_Agent = GetComponent<AStarAgent>();
         //transform.position = pointA.position;
         //StartCoroutine(Coroutine_MoveAB());
+
+        curWaypoint = 0;
     }
 
-    IEnumerator Coroutine_MoveAB()
-    {
-        yield return null;
-        while (true)
-        {
-            _Agent.Pathfinding(pointB.position);
-            while (_Agent.Status == AStarAgentStatus.Invalid)
-            {
-                Transform pom1 = pointA;
-                pointA = pointB;
-                pointB = pom1;
-                transform.position = pointA.position;
-                _Agent.Pathfinding(pointB.position);
-                yield return new WaitForSeconds(0.2f);
-            }
-            while (_Agent.Status != AStarAgentStatus.Finished)
-            {
-                yield return null;
-            }
-            Transform pom = pointA;
-            pointA = pointB;
-            pointB = pom;
-            yield return null;
-        }
-    }
+    //IEnumerator Coroutine_MoveAB()
+    //{
+    //    yield return null;
+    //    while (true)
+    //    {
+    //        _Agent.Pathfinding(pointB.position);
+    //        while (_Agent.Status == AStarAgentStatus.Invalid)
+    //        {
+    //            Transform pom1 = pointA;
+    //            pointA = pointB;
+    //            pointB = pom1;
+    //            transform.position = pointA.position;
+    //            _Agent.Pathfinding(pointB.position);
+    //            yield return new WaitForSeconds(0.2f);
+    //        }
+    //        while (_Agent.Status != AStarAgentStatus.Finished)
+    //        {
+    //            yield return null;
+    //        }
+    //        Transform pom = pointA;
+    //        pointA = pointB;
+    //        pointB = pom;
+    //        yield return null;
+    //    }
+    //}
 
     //Should probably find the racecontroller in the onenable function and add it to the end of the waypoint list
     //That way we dont have to manually drag in the racecontroller every time
@@ -60,10 +66,16 @@ public class RaceEnemy : MonoBehaviour
 
     private void Update()
     {
-        //float distance = Vector3.Distance(transform.position, waypoints[curWaypoint].transform.position);
-        //if (distance < waypointProx && curWaypoint < waypoints.Length) curWaypoint++;
-        //
-        //if (curWaypoint > 0 && curWaypoint < waypoints.Length) transform.LookAt(waypoints[curWaypoint].transform, Vector3.up);
-        //bod.AddForce(transform.up * moveSpd * Time.deltaTime);
+        distance = Vector3.Distance(transform.position, waypoints[curWaypoint].transform.position);
+        if (distance < waypointProx && curWaypoint < waypoints.Length) curWaypoint++;
+
+        if (curWaypoint >= 0 && curWaypoint < waypoints.Length)
+        {
+            Quaternion lookRot = Quaternion.LookRotation((waypoints[curWaypoint].transform.position - transform.position).normalized);
+            //transform.rotation = Quaternion.Slerp(transform.rotation, lookRot, turnSpd * Time.deltaTime);
+            transform.rotation = lookRot;
+        }
+        //if (curWaypoint >= 0 && curWaypoint < waypoints.Length) transform.LookAt(waypoints[curWaypoint].transform, new Vector3(-1, 0, 1));
+        bod.AddForce(transform.forward * moveSpd * Time.deltaTime);
     }
 }
