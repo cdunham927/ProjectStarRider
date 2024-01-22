adad00965cf5519ce4b1732f89bc5fb02768f429
ZionJones <Joneszion412@gmail.com>
2024-01-22 03:13:51 -0800

    new wall vfx

--- Assets/Scripts/Race/DestructibleObject.cs
+++ Assets/Scripts/Race/DestructibleObject.cs
@@ -2,17 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using Cinemachine;
+
 public class DestructibleObject : MonoBehaviour
 {
     public float hp;
+    [Header(" Assigned Destroyed Object : ")]
     public GameObject objToDestroy;
 
+    //public AnimationClip deathClip;
+    [Header(" Attached Particle Systems : ")]
+    public GameObject deathVFX;
+   
+    [Header("Camera Shake Settings: ")]
+    //Camera shake on take damage
+    CinemachineVirtualCamera cine;
+    CinemachineBasicMultiChannelPerlin perlin;
+    public float shakeTimer = 0.1f;
+    float curTime;
+    public float shakeAmt = .5f;
+    
     public void TakeDamage(float amt)
     {
         hp -= amt;
 
         if (hp <= 0)
         {
+            ShakeCamera();
+            
+        
+
+            Instantiate(deathVFX, transform.position, transform.rotation);
+            Invoke("Disable", 0.01f);
             Die();
         }
     }
@@ -24,5 +45,14 @@ public class DestructibleObject : MonoBehaviour
             objToDestroy.SetActive(false);
         }
         else gameObject.SetActive(false);
+    }
+
+    public void ShakeCamera()
+    {
+        if (perlin != null)
+        {
+            perlin.m_AmplitudeGain = shakeAmt;
+            curTime = shakeTimer;
+        }
     }
 }
