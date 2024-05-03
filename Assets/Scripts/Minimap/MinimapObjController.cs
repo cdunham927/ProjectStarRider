1eec994414132e7427ad491f0b0f29d5b37a59bb
cdunham927 <cdunham927@gmail.com>
2024-05-02 17:19:42 -0700

    New Ship prefab

--- Assets/Scripts/Minimap/MinimapObjController.cs
+++ Assets/Scripts/Minimap/MinimapObjController.cs
@@ -9,25 +9,32 @@ public class MinimapObjController : MonoBehaviour
     Vector2 sizeMod;
     public Vector3 posOffset;
     public Vector3 rotationOffset;
+    public bool reparent = true;
 
     private void Awake()
     {
         par = transform.parent;
         sizeMod = transform.parent.localScale;
-        transform.SetParent(null);
-        transform.localScale = new Vector3(size * sizeMod.x, size * sizeMod.y, 1);
+        if (reparent)
+        {
+            transform.SetParent(null);
+            transform.localScale = new Vector3(size * sizeMod.x, size * sizeMod.y, 1);
+        }
     }
 
     private void Update()
     {
-        if (par.gameObject.activeInHierarchy)
+        if (reparent)
         {
-            transform.position = new Vector3(par.position.x + posOffset.x, posOffset.y, par.position.z + posOffset.z);
-            transform.rotation = Quaternion.Euler(90, par.eulerAngles.y, par.eulerAngles.z + rotationOffset.z);
-        }
-        else
-        {
-            gameObject.SetActive(false);
+            if (par.gameObject.activeInHierarchy)
+            {
+                transform.position = new Vector3(par.position.x + posOffset.x, par.position.y + posOffset.y, par.position.z + posOffset.z);
+                transform.rotation = Quaternion.Euler(90, par.eulerAngles.y, par.eulerAngles.z + rotationOffset.z);
+            }
+            else
+            {
+                gameObject.SetActive(false);
+            }
         }
     }
 }
