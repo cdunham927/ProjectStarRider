72ce4dc63e38f799ce5b12bd4273f8b588c0eb80
cdunham927 <cdunham927@gmail.com>
2024-10-07 18:08:56 -0700

    Test Enemy Indicator, about 50% working

--- Assets/Scripts/Enemies/EnemyIndicator.cs
+++ Assets/Scripts/Enemies/EnemyIndicator.cs
@@ -1,30 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MPUIKIT;
 
 public class EnemyIndicator : MonoBehaviour
 {
-    MeshRenderer rend;
-    [HideInInspector]
+    //MeshRenderer rend;
     public bool parentVisible;
+    GameManager cont;
+    RectTransform contRect;
+    public Vector2 screenSpace;
+    public Vector2 viewportSpace;
+    public Vector2 desiredPos;
+    Transform player;
+    RectTransform rect;
+
+    //Enemy to follow
+    public GameObject enemy;
+
+    [Range(0f, 0.2f)]
+    public float xClampMin;
+    [Range(0.8f, 1f)]
+    public float xClampMax;
+    [Range(0f, 0.2f)]
+    public float yClampMin;
+    [Range(0.8f, 1f)]
+    public float yClampMax;
+    public float actualClampX;
+    public float actualClampY;
+
+    MPImage img;
 
     private void Awake()
     {
-        rend = GetComponent<MeshRenderer>();
+        img = GetComponent<MPImage>();
+        rect = GetComponent<RectTransform>();
+        //rend = GetComponent<MeshRenderer>();
+        player = FindObjectOfType<PlayerController>().transform;
+
+        actualClampX = Screen.width + xClampMax;
+        actualClampY = Screen.height + yClampMax;
     }
 
     private void Update()
     {
-        if (parentVisible)
+        screenSpace = Camera.main.WorldToScreenPoint(enemy.transform.position);
+        viewportSpace = Camera.main.WorldToViewportPoint(enemy.transform.position);
+
+        if (player == null)
         {
-            rend.enabled = false;
+            player = FindObjectOfType<PlayerController>().transform;
         }
-        else
-        {
-            //Raycast or whatever the fuck to see where to put the indicator
-            rend.enabled = true;
 
+        if (player != null) {
 
+            if (parentVisible)
+            {
+                img.enabled = false;
+            }
+            else
+            {
+
+                img.enabled = true;
+
+                desiredPos = screenSpace;
+                desiredPos.x = Mathf.Clamp(desiredPos.x, xClampMin, actualClampX);
+                desiredPos.y = Mathf.Clamp(desiredPos.y, yClampMin, actualClampY);
+                rect.position = desiredPos;
+            }
         }
     }
 }
