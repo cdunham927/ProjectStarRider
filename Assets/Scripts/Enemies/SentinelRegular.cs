babb611f8dc914f25e6155eb8abde95ad2bfafd1
cdunham927 <cdunham927@gmail.com>
2024-10-23 02:27:29 -0700

    Omni directional bullet pattern

--- Assets/Scripts/Enemies/SentinelRegular.cs
+++ Assets/Scripts/Enemies/SentinelRegular.cs
@@ -4,11 +4,28 @@ using UnityEngine;
 
 public class SentinelRegular : EnemyControllerBase
 {
+    public bool patternB = false;
+    public bool patternC = false;
+
     protected override void Alert()
     {
         //If the cooldown is at 0 then we can attack
         //if (attackCools <= 0) Attack();
-        if (attackCools <= 0) RadialAttack();
+        if (attackCools <= 0)
+        {
+            if (patternB)
+            {
+                PatternBAttack();
+            }
+            else if (patternC)
+            {
+                PatternCAttack();
+            }
+            else
+            {
+                RadialAttack();
+            }
+        }
     }
 
     protected override void Attack()
@@ -40,6 +57,73 @@ public class SentinelRegular : EnemyControllerBase
         attackCools = timeBetweenAttacks;
         
         ChangeState(enemystates.alert);
+    }
+
+    //Shoot in x, y, and z directions
+    void PatternBAttack()
+    {
+        if (bulletPool == null) bulletPool = cont.enemyBulPool;
+        float angle = 0;
+        float increment = 360 / numBullets;
+        for (int i = 0; i < numBullets; i++)
+        {
+            //Get pooled bullet
+            GameObject bul = bulletPool.GetPooledObject();
+            if (bul != null)
+            {
+                //Put it where the enemy position is
+                bul.transform.position = transform.position;
+                //Aim it at the player
+                //Activate it at the enemy position
+                bul.SetActive(true);
+                bul.transform.rotation = Quaternion.identity;
+
+                angle += increment;
+
+                bul.transform.rotation = Quaternion.Euler(0, angle, 0);
+                bul.GetComponent<EnemyBullet>().Push();
+            }
+
+            //Get pooled bullet
+            GameObject bulX = bulletPool.GetPooledObject();
+            if (bulX != null)
+            {
+                //Put it where the enemy position is
+                bulX.transform.position = transform.position;
+                //Aim it at the player
+                //Activate it at the enemy position
+                bulX.SetActive(true);
+                bulX.transform.rotation = Quaternion.identity;
+
+                bulX.transform.rotation = Quaternion.Euler(angle, 0, 0);
+                bulX.GetComponent<EnemyBullet>().Push();
+            }
+
+            //Get pooled bullet
+            GameObject bulZ = bulletPool.GetPooledObject();
+            if (bulZ != null)
+            {
+                //Put it where the enemy position is
+                bulZ.transform.position = transform.position;
+                //Aim it at the player
+                //Activate it at the enemy position
+                bulZ.SetActive(true);
+                bulZ.transform.rotation = Quaternion.identity;
+
+                bulZ.transform.rotation = Quaternion.Euler(angle, angle, 0);
+                bulZ.GetComponent<EnemyBullet>().Push();
+            }
+        }
+
+        //Reset attack cooldown
+        attackCools = timeBetweenAttacks;
+
+        ChangeState(enemystates.alert);
+    }
+
+    void PatternCAttack()
+    {
+
     }
 
     public int numBullets = 30;
