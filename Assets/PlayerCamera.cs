2d18935824642e72a9ba019b856c670d10f37aef
ZionJones <Joneszion412@gmail.com>
2024-12-05 16:58:16 -0800

    player camera script

--- Assets/PlayerCamera.cs
+++ Assets/PlayerCamera.cs
@@ -7,12 +7,27 @@ using System;
 
 public class PlayerCamera : MonoBehaviour
 {
-    // Get the Cinemachine Brain component
-    void awake()
-    {
-    CinemachineBrain brain = Camera.main.GetComponent<CinemachineBrain>();
-    CinemachineVirtualCamera specificCamera = GameObject.Find("MyVirtualCamera").GetComponent<CinemachineVirtualCamera>();
+    /// <summary>
+    /// The puyrpose of this script is to affect the cinmachine camera when the player does certain actions
+    /// The player camera is attached to the 'Ship' Prefab under 'StarRider'
+    /// 1.) The first goal is to adjsut the camrea field of view to sell the speed of the players movement
+    /// have the script find the player camera and the cinamachine brain
+    /// the script need to fina the Cinemachine add on "Cinemachine Follow Zoom Script" that is attatched to the player camera
+    /// </summary>
+    /// 
 
+    // Get the Cinemachine Brain component
+    public CinemachineVirtualCamera playercamera;
+
+    private void Awake()
+    {
+        CinemachineBrain brain = Camera.main.GetComponent<CinemachineBrain>();
+        CinemachineVirtualCamera specificCamera = GameObject.Find("MyVirtualCamera").GetComponent<CinemachineVirtualCamera>();
+        if (brain == null)
+        {
+            brain = Camera.main.gameObject.AddComponent<CinemachineBrain>();
+
+        }
     }
     
 }
