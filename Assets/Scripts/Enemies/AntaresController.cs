22e6c7f21356dd627e464981f9cab1393065a7c5
cdunham927 <cdunham927@gmail.com>
2025-02-13 18:57:10 -0800

    Antares base controller set up

---
+++ Assets/Scripts/Enemies/AntaresController.cs
@@ -0,0 +1,297 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AntaresController : BossControllerBase
+{
+    //notifications
+    [Header("Player Objective notifactions: ")]
+    public GameObject[] _notifications;
+
+    float curHpLoss = 0;
+
+    //Antares enemy phases
+    [Header(" Phase 1 Settings : ")]
+    public bool phaseOneLossAttack = false;
+    public float phaseOneLossPerc = 0.20f;
+    float pOLA;
+
+    //Phase 2 attack and %
+    [Header(" Phase 2 Settings : ")]
+    public bool phaseTwoLossAttack = false;
+    public float phaseTwoLossPerc = 0.15f;
+    float pTLA;
+
+    //Final phase attack info
+    [Header(" Phase 3 Settings : ")]
+    public bool phaseThreeLossAttack = false;
+    public float phaseThreeLossPerc = 0.10f;
+    float pTtLA;
+
+    public ObjectPool homingBulletPool;
+    public int numBullets = 8;
+    public GameObject bulSpawn;
+
+    public Dialogue thirdPhaseDialogue;
+
+    [Header("Audio Clips: ")]
+    public AudioClip[] PlayerSfx;
+    private AudioSource AS;
+
+    public float startAttackCools = 15f;
+
+    public bool isAttacking = false;
+
+    public Transform[] waypoints; // Array of waypoint transforms
+    public float spd;
+    private int currentWaypointIndex = 0;
+    public float pathChangeDistance = 2f;
+    public float rotSpd = 15f;
+
+    //Aniamtion State  make sure string match name of animations
+    //const string Cetus_Reflect = "Armature|Reflect";
+    //const string Cetus_Idle = "Armature|Idle";
+    //const string Cetus_Attack_1 = "Armature|Attack1";
+    //const string Cetus_Attack_2 = "Armature|Attack2";
+    //const string Cetus_Wings_R = "Armature|Idle";
+    //const string Cetus_Wings_L = "Armature|Idle";
+    //const string Cetus_Wings_All = "CetusArmature|WingSlaps_All";
+    //const string Cetus_Bite = "CetusArmature|BiteBack";
+    //const string Cetus_Tail_Swipe = "Armature|TailWhip";
+    //const string Cetus_Dive = "Armature|Idle";
+    //const string Cetus_Flustered = "Armature|Idle";
+    //const string Cetus_Death = "Armature|Death";
+    //const string Cetus_Roar = "CetusArmature|Roaring";
+    //const string Cetus_Charge_Laser = "Charge Attack Edit";
+    //const string Cetus_Charging = "Charge Attack Edit";
+    //const string Cetus_Whirlwind = "CetusArmature|Whirlwind";
+
+
+
+    protected override void Awake()
+    {
+        //Boss does a special attack after losing a set amount of health per phase
+        pOLA = maxHp * phaseOneLossPerc;
+        pTLA = maxHp * phaseTwoLossPerc;
+        pTtLA = maxHp * phaseThreeLossPerc;
+
+        base.Awake();
+
+        AS = GetComponent<AudioSource>();
+        //Finds Attached Animator to the player
+        anim = GetComponentInChildren<Animator>();
+
+        attackCools = startAttackCools;
+    }
+
+    protected override void OnEnable()
+    {
+        player = FindObjectOfType<PlayerController>();
+        //detection = GetComponentInChildren<BossDetectionController>();
+        detectionCollider.radius = attackRange;
+
+        base.OnEnable();
+    }
+
+    protected override void Update()
+    {
+        if (!isAttacking)
+        {
+            //Move along our circular looping path
+            // Calculate direction to next waypoint
+            Vector3 direction = (waypoints[currentWaypointIndex].position - transform.position).normalized;
+            //Look in direction of waypoint
+            var lookRotation = Quaternion.LookRotation(waypoints[currentWaypointIndex].position - transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotSpd);
+            // Move towards the waypoint
+            transform.position += direction * spd * Time.deltaTime;
+            // Check if reached the waypoint
+            if (Vector3.Distance(transform.position, waypoints[currentWaypointIndex].position) < pathChangeDistance)
+            {
+                currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length; // Loop back to the beginning
+            }
+        }
+
+        //If the cooldown is at 0 then we can attack
+        if (attackCools <= 0) Attack();
+        //If the cooldown is greater than 0 we decrement it every frame
+        if (attackCools > 0) attackCools -= Time.deltaTime;
+
+
+        if (Application.isEditor)
+        {
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                Damage((int)maxHp / 2);
+            }
+        }
+    }
+
+    protected override void Attack()
+    {
+        //If the player is close enough
+        //We can probably use this to determine if we do a scorpion tail attack or shoot bullets at the player
+        if (playerInRange && curHp > 0 & player != null && pStats != null && pStats.Curr_hp > 0)
+        {
+            AttackOne();
+        }
+        else
+        {
+            AttackTwo();
+        }
+    }
+
+    //Ranged Attack
+    protected override void AttackOne()
+    {
+        Invoke("RangedAttack", 0.85f);
+
+        //Reset attack cooldown
+        attackCools = atkCooldowns[0];
+    }
+
+    //Melee Attack
+    protected override void AttackTwo()
+    {
+        Invoke("MeleeAttack", 1.15f);
+
+        //Reset attack cooldown
+        attackCools = atkCooldowns[1];
+    }
+
+    void MeleeAttack()
+    {
+
+    }
+
+    void RangedAttack()
+    {
+
+    }
+
+    void SpawnBullets()
+    {
+        for (int i = 0; i < numBullets; i++)
+        {
+            //Spawn at every other position(so we dont have 50 bullets spawn)
+            //if (i % 2 == 0)
+            //{
+            GameObject bul = homingBulletPool.GetPooledObject();
+            if (bul != null)
+            {
+                //Put it where the enemy position is
+                bul.transform.position = bulSpawn.transform.position;
+                bul.transform.Rotate(Random.Range(-accx, accx), Random.Range(-accy, accy), 0);
+
+                //Aim it at the player
+                //
+                //var offset = 0f;
+                //Vector2 direction = player.transform.position - t.transform.position;
+
+                //direction.Normalize();
+                //float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+                //Activate it at the enemy position
+                bul.SetActive(true);
+                //bul.transform.rotation = Quaternion.Euler(Vector3.forward * (angle + offset));
+                //bul.transform.forward = Vector3.forward * (angle + offset);
+                bul.transform.rotation = Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360));
+                //bul.GetComponent<EnemyBullet>().Push();
+            }
+            //}
+        }
+        AS.PlayOneShot(PlayerSfx[1]);
+
+        ChangeState(enemystates.alert);
+    }
+
+    public void AntaresDeath()
+    {
+        skinnedMeshRenderer.material.color = Color.white;
+        Death();
+    }
+
+    public override void Damage(int damageAmount)
+    {
+        hpBar.SwitchUIActive(true);
+        curHp -= damageAmount;
+        if (curHp > 0) DamageBlink();
+
+        //Check for phase change
+        if (curHp < phase3Thres)
+        {
+            currentPhase = 3;
+            return;
+        }
+        else if (curHp < phase2Thres)
+        {
+            currentPhase = 2;
+            return;
+        }
+        else currentPhase = 1;
+
+        curHpLoss += damageAmount;
+
+        switch (currentPhase)
+        {
+            case 3:
+                if (curHpLoss > pTtLA)
+                {
+                    //AttackThree();
+                    curHpLoss = 0;
+                }
+                break;
+            case 2:
+                if (curHpLoss > pTLA)
+                {
+                    //AttackThree();
+                    curHpLoss = 0;
+                }
+                break;
+            case 1:
+                if (curHpLoss > pOLA)
+                {
+                    //AttackThree();
+                    curHpLoss = 0;
+                }
+                break;
+        }
+
+        if (curHp <= 0)
+        {
+            if (minimapObj != null) minimapObj.SetActive(false);
+            if (manager != null) manager.EnemyDied();
+            AntaresDeath();
+
+            Instantiate(deathVFX, transform.position, transform.rotation);
+            Invoke("Disable", deathTime);
+        }
+    }
+
+
+    void PatternCAttack()
+    {
+        if (bulletPool == null) bulletPool = cont.enemyBulPool;
+        float angle = 0;
+        float increment = 360 / numBullets;
+        for (int i = 0; i < numBullets; i++)
+        {
+            //Get pooled bullet
+            GameObject bul = bulletPool.GetPooledObject();
+            if (bul != null)
+            {
+                //Put it where the enemy position is
+                bul.transform.position = transform.position;
+                //Aim it at the player
+                //Activate it at the enemy position
+                bul.SetActive(true);
+                bul.transform.rotation = Quaternion.identity;
+
+                angle += increment;
+
+                bul.transform.rotation = Quaternion.Euler(0, angle, 0);
+                bul.GetComponent<EnemyBullet>().Push();
+            }
+        }
+    }
+}
--- Assets/Scripts/Enemies/BossControllerBase.cs
+++ Assets/Scripts/Enemies/BossControllerBase.cs
@@ -77,27 +77,6 @@ public class BossControllerBase : EnemyControllerBase
                     break;
             }
         }
-        ////Phase 3 attack
-        //if (curHp < phase3Thres)
-        //{
-        //    if (v < chanceForAtkFour) AttackFour();
-        //    else if (v < chanceForAtkThree) AttackThree();
-        //    else if (v < chanceForAtkTwo) AttackTwo();
-        //    else AttackOne();
-        //}
-        ////Phase 2 attack
-        //else if (curHp < phase2Thres)
-        //{
-        //    if (v < chanceForAtkThree) AttackThree();
-        //    else if (v < chanceForAtkTwo) AttackTwo();
-        //    else AttackOne();
-        //}
-        ////Choice of 2 attacks for phase 1
-        //else
-        //{
-        //    if (v < chanceForAtkTwo) AttackTwo();
-        //    else AttackOne();
-        //}
     }
 
     protected override void Death()
