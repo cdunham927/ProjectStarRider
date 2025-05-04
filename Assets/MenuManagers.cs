4ef7fbcf4e9ecf7b41edb8a6c925d18bf2e402d0
ZionJones <Joneszion412@gmail.com>
2025-05-03 18:23:01 -0700

    fix ui and audio

---
+++ Assets/MenuManagers.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManagers : MonoBehaviour
+{
+    [Header("Menu Objects")]
+    [SerializeField] private GameObject _mainMenuCanvas;
+    [SerializeField] private GameObject _settingsMenuMenuCanvas;
+
+    [Header("Player Scripts to Deactivate onn Pause ")]
+    [SerializeField] private GameObject _Player;
+    [SerializeField] private GameObject _playerAttack;
+
+    [Header("First Selected Options")]
+    [SerializeField] private GameObject _mainMenuFirst;
+    [SerializeField] private GameObject _settingsMenuFirst;
+
+
+    private bool ispaused;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _mainMenuCanvas.SetActive(false);
+        _settingsMenuMenuCanvas.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
