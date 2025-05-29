4cb31fe187224118eedc7584e575fa9afd19d6cc
cdunham927 <cdunham927@gmail.com>
2025-05-28 18:41:05 -0700

    Cetus weakpoint flash on damage

--- Assets/Scripts/Enemies/BossHitPointController.cs
+++ Assets/Scripts/Enemies/BossHitPointController.cs
@@ -19,60 +19,54 @@ public class BossHitPointController : MonoBehaviour
     //public Material[] mats;
     public GameObject deadWeakPoint;
 
+    public SkinnedMeshRenderer skinnedMeshRenderer;
+    Material origMat;
+    Material hitMat;
+    public float blinkDuration = 0.2f;
+
     private void Awake()
     {
         boss = FindObjectOfType<BossControllerBase>();
         breakHp = (boss.maxHp * (breakPercentage / 100));
-    }
 
-    //private void Update()
-    //{
-    //    //if (Input.GetKeyDown(KeyCode.Alpha5))
-    //    //{
-    //    //    Material[] tempMats = skinnedMeshRenderers.materials;
-    //    //    tempMats[0] = mat;
-    //    //    skinnedMeshRenderers.materials = tempMats;
-    //    //}
-    //
-    //    if (Input.GetKeyDown(KeyCode.N)) Damage(25);
-    //}
+        hitMat = new Material(Shader.Find("Specular"));
+        hitMat.color = Color.red;
+
+        if (skinnedMeshRenderer == null) skinnedMeshRenderer = GetComponentInChildren<SkinnedMeshRenderer>();
+    }
 
     public void Damage(int amt)
     {
-        if (!boss.barrier.gameObject.activeInHierarchy) { 
-        //Calculate Damage
-        int totDmg = Mathf.RoundToInt((float)amt * dmgMult);
-        boss.Damage(totDmg);
-        //Blow up the weakpoint if it takes too much damage;
-        curDmg += totDmg;
+        if (!boss.barrier.gameObject.activeInHierarchy) 
+        { 
+            //Calculate Damage
+            int totDmg = Mathf.RoundToInt((float)amt * dmgMult);
+            boss.Damage(totDmg);
+
+            //Blow up the weakpoint if it takes too much damage;
+            curDmg += totDmg;
             if (curDmg >= breakHp)
             {
                 deadWeakPoint.SetActive(true);
-                //if (matNum == 0)
-                //{
-                //    Material[] tempMats = skinnedMeshRenderers.materials;
-                //    tempMats[0] = mat;
-                //    skinnedMeshRenderers.materials = tempMats;
-                //    deadWeakPoint.SetActive(false);
-                //}
-                //else
-                //{
-                //    Material[] tempMats = skinnedMeshRenderers.materials;
-                //    tempMats[2] = mat;
-                //    skinnedMeshRenderers.materials = tempMats;
-                //    //Debug.Log("Changing front mat");
-                //}
-                //Debug.Log("Changing material");
-                //curMaterial = deadMaterial;
-                //boss.ChangeMaterial();
+
                 GetComponent<Collider>().enabled = false;
-                //gameObject.SetActive(false);
             }
         }
+    }
 
-        //Retaliate against attack
-        //boss.retaliatePos = transform.position;
-        //boss.Retaliate();
+    protected void DamageBlink()
+    {
+        if (skinnedMeshRenderer != null)
+        {
+            skinnedMeshRenderer.material = hitMat;
+
+            Invoke("ResetMaterial", blinkDuration);
+        }
+    }
+
+    void ResetMaterial()
+    {
+        if (skinnedMeshRenderer != null) skinnedMeshRenderer.material = origMat;
     }
 
     public void Retaliate()
