9b104e3a3535f7fd860a56c49af8d544038fe765
cdunham927 <cdunham927@gmail.com>
2025-08-07 18:39:06 -0700

    Occlusion baking for Antares and Cetus levels

--- Assets/Scripts/Enemies/EnemyBullets/BulletPatterns.cs
+++ Assets/Scripts/Enemies/EnemyBullets/BulletPatterns.cs
@@ -37,7 +37,35 @@ public class BulletPatterns : MonoBehaviour
     }
 
     //Shoots bullets forward at the player like a shotgun
-   public void ShotgunPattern(ObjectPool pool, int numBullets = 16)
+    public void ShotgunPattern(ObjectPool pool, int numBullets = 16)
+    {
+        if (pool == null) return;
+
+        float angle = 0;
+        float increment = 360 / numBullets;
+        for (int i = 0; i < numBullets; i++)
+        {
+            //Get pooled bullet
+            GameObject bul = pool.GetPooledObject();
+            if (bul != null)
+            {
+                //Put it where the enemy position is
+                bul.transform.position = transform.position;
+                //Aim it at the player
+                //Activate it at the enemy position
+                bul.SetActive(true);
+                bul.transform.rotation = Quaternion.identity;
+
+                angle += increment;
+
+                bul.transform.rotation = Quaternion.Euler(0, angle, 0);
+                bul.GetComponent<EnemyBullet>().Push();
+            }
+        }
+    }
+
+    //Shoots bullets forward at the player like a shotgun
+    public void VerticalPattern(ObjectPool pool, int numBullets = 16)
     {
         if (pool == null) return;
 
