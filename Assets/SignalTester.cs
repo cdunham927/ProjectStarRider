b0a44efc92fc1bb234e248de7c02db13e8769d50
ZionJones <Joneszion412@gmail.com>
2025-12-11 18:11:18 -0800

    signal tester code

---
+++ Assets/SignalTester.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Playables;
+using Cinemachine;
+
+public class SignalTester : MonoBehaviour
+{
+
+    public GameObject player;
+    public bool canmove = true;
+
+    void Start()
+    {
+        
+
+
+    }
+
+
+    public void StopMovement() 
+    { 
+    
+    
+    
+    }
+}
